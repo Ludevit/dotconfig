3c6ed724a05306b6ca8f97d7bfd5a620a805d8fd
Ludevit <68378933+Ludevit@users.noreply.github.com>
2023-10-09 01:24:17 +0200

    added simple install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+pathrc=$HOME/.config/nano
+
+red_color="\e[31m"
+green_color="\e[32m"
+yellow_color="\e[33m"
+end_color="\e[0m"
+
+# DEBUG echo "path to nano is: $pathrc"
+
+# Checks for nanorc and user is promtmted to install nanorc
+if [ -f $pathrc/nanorc  ]
+then
+	# Tells user that there alredy is a nanorc in ~/.config/nano
+	echo -e "${yellow_color}There is already nanorc in your config directory.${end_color}"
+	echo -e "${yellow_color}Please check: $pathrc ${end_color}"
+else
+	# If there is no nanorc user has a choise to install nanorc
+	# DEBUG echo -e "${red_color}There is NO nanorc file.${end_color}"
+	echo "do you want to install GNU Nano configuration file? [y/N]"
+	read -n 1 -s install_input
+	if [ "$install_input" == "y" ]
+	then
+		#TODO check what happends when i delete nano folder
+		echo "installing nanorc..."
+		mkdir -v $pathrc  && cp -v ./nanorc $pathrc/nanorc
+		[ $? == 0 ] && echo -e "${green_color}It's Done. Enjoy your new GNU nano config.${end_color}" || echo -e "${red_color}installing failed at copying files over to $pathrc${end_color}"
+	fi
+fi
+
+exit
